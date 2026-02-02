869a7708eea7f8a9417dfbfb5aa6c4f6c2e68ca5
Frank Spitzock <157513226+FSpitzock@users.noreply.github.com>
2026-02-01 21:24:25 -0500

    updates to Answers.ts file

--- server/src/models/Answer.ts
+++ server/src/models/Answer.ts
@@ -7,6 +7,12 @@ import Question from './Question';
 // Hint: Answer should have id, body, questionId, userId, createdAt, updatedAt
 interface AnswerAttributes {
   // TODO: Add properties here
+   id: number
+  body: string
+  questionId: number
+  userId: number
+  createdAt?: Date
+  updatedAt?: Date
 }
 
 interface AnswerCreationAttributes extends Optional<AnswerAttributes, 'id'> {}
@@ -14,6 +20,13 @@ interface AnswerCreationAttributes extends Optional<AnswerAttributes, 'id'> {}
 // TODO: Create the Answer class extending Model
 class Answer extends Model<AnswerAttributes, AnswerCreationAttributes> implements AnswerAttributes {
   // TODO: Declare public properties
+  public id!: number
+  public body!: string
+  public questionId!: number
+  public userId!: number
+
+  public readonly createdAt!: Date
+  public readonly updatedAt!: Date
 }
 
 // TODO: Initialize the Answer model
@@ -25,6 +38,37 @@ Answer.init(
     // - body should be TEXT type and not null
     // - questionId should reference questions table
     // - userId should reference users table
+
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [10, 10000]
+      }
+    },
+    questionId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'questions',
+        key: 'id'
+      },
+      onDelete: 'CASCADE'
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'users',
+        key: 'id'
+      },
+      onDelete: 'CASCADE'
+    }
   },
   {
     sequelize,
@@ -38,5 +82,24 @@ Answer.init(
 // Hint: An Answer belongs to a Question
 // Hint: A Question can have many Answers
 // Hint: A User can have many Answers
+
+Answer.belongsTo(User, {
+  foreignKey: 'userId',
+  as: 'author'
+})
+
+Answer.belongsTo(Question, {
+  foreignKey: 'questionId'
+})
+
+Question.hasMany(Answer, {
+  foreignKey: 'questionId',
+  as: 'answers'
+})
+
+User.hasMany(Answer, {
+  foreignKey: 'userId',
+  as: 'answers'
+})
 
 export default Answer;
