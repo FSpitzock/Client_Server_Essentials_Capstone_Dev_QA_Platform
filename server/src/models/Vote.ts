3cd7e092d953c3181a1eb331053ceeb2726e6a58
Frank Spitzock <157513226+FSpitzock@users.noreply.github.com>
2026-02-01 21:47:13 -0500

    updates to Vote.ts file

--- server/src/models/Vote.ts
+++ server/src/models/Vote.ts
@@ -7,6 +7,12 @@ import Answer from './Answer';
 // Hint: Vote should have id, value, answerId, userId, createdAt
 interface VoteAttributes {
   // TODO: Add properties here
+    id: number;
+  value: number; // 1 = upvote, -1 = downvote
+  answerId: number;
+  userId: number;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
 interface VoteCreationAttributes extends Optional<VoteAttributes, 'id'> {}
@@ -14,6 +20,13 @@ interface VoteCreationAttributes extends Optional<VoteAttributes, 'id'> {}
 // TODO: Create the Vote class extending Model
 class Vote extends Model<VoteAttributes, VoteCreationAttributes> implements VoteAttributes {
   // TODO: Declare public properties
+  public id!: number;
+  public value!: number;
+  public answerId!: number;
+  public userId!: number;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
 }
 
 // TODO: Initialize the Vote model
@@ -26,6 +39,34 @@ Vote.init(
     // - answerId should reference answers table
     // - userId should reference users table
     // - Add validation to ensure value is only 1 or -1
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    value: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isIn: [[1, -1]]
+      }
+    },
+    answerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'answers',
+        key: 'id'
+      }
+    },
+     userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'users',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
@@ -34,10 +75,10 @@ Vote.init(
     indexes: [
       // TODO: Add unique index for userId + answerId
       // This prevents a user from voting multiple times on same answer
-      // {
-      //   unique: true,
-      //   fields: ['userId', 'answerId']
-      // }
+       {
+         unique: true,
+         fields: ['userId', 'answerId']
+       }
     ]
   }
 );
@@ -46,5 +87,9 @@ Vote.init(
 // Hint: A Vote belongs to a User
 // Hint: A Vote belongs to an Answer
 // Hint: An Answer can have many Votes
+Vote.belongsTo(User, { foreignKey: 'userId' });
+Vote.belongsTo(Answer, { foreignKey: 'answerId' });
 
+User.hasMany(Vote, { foreignKey: 'userId' });
+Answer.hasMany(Vote, { foreignKey: 'answerId' });
 export default Vote;
